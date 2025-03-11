fba9a906e7f72ed35beed6ec63fe061236ca6627
iamnotluka <zoricluk@gmail.com>
2025-03-11 15:54:59 +1000

    Add splash cursor

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { CssBaseline, GeistProvider, Tabs, Text } from "@geist-ui/core";
 import "./App.css";
 import Landing from "./Landing";
 import "inter-ui/inter.css";
+import SplashCursor from "./components/SplashCursor";
 
 function App() {
 	return (
 		<GeistProvider themeType="dark">
+			<SplashCursor />
 			<CssBaseline />
 			<Landing />
 		</GeistProvider>
