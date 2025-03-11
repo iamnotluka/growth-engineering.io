273bffe0f5c35e30485d365fa079005932bfa4df
iamnotluka <zoricluk@gmail.com>
2025-03-11 22:13:55 +1000

    Adding login handler

--- src/TopNav.tsx
+++ src/TopNav.tsx
@@ -1,12 +1,36 @@
-import { Button, Grid, Text, Tooltip } from "@geist-ui/core";
+import { Button, Grid, Modal, Text } from "@geist-ui/core";
 import { LogIn } from "@geist-ui/icons";
 import { useState } from "react";
 
 const TopNav = () => {
+	const [state, setState] = useState(false);
+	const handler = () => setState(true);
 	const [loginLoading, setLoginLoading] = useState(false);
 	const [contactLoading, setContactLoading] = useState(false);
+
+	const closeHandler = () => {
+		setState(false);
+		setLoginLoading(false);
+	};
 	return (
 		<nav className="flex justify-between items-center bg-black px-[16px] h-[64px] border-b border-b-[#333]">
+			<Modal visible={state} onClose={closeHandler}>
+				<Modal.Content>
+					<Text className="text-center text-xs">
+						Comprehensive toolkit enabling e-commerce brand owners, founders and operators to scale.
+					</Text>
+				</Modal.Content>
+				<Modal.Title>
+					<Text className="font-bold">GE Analytics</Text>
+				</Modal.Title>
+				{/* <Modal.Subtitle>Coming soon...</Modal.Subtitle> */}
+
+				<Modal.Content>
+					<Text className="text-center text-xs">
+						<span className="text-[#999]">COMING SOON...</span>
+					</Text>
+				</Modal.Content>
+			</Modal>
 			<div className="flex items-center">
 				<span>
 					<div className="flex items-center space-x-2">
@@ -18,32 +42,27 @@ const TopNav = () => {
 			</div>
 			<div className="flex items-center">
 				<Grid.Container gap={1.5}>
-					<Tooltip trigger="hover" text="Coming soon.." placement="bottom">
-						<Grid>
-							<div className="block">
-								<Button
-									type="default"
-									auto
-									scale={0.7}
-									disabled
-									placeholder={undefined}
-									onPointerEnterCapture={undefined}
-									onPointerLeaveCapture={undefined}
-									icon={<LogIn />}
-									loading={loginLoading}
-									onClick={() => {
-										setLoginLoading(true);
-										window.location.href = "https://app.growth-engineering.io";
-										setTimeout(() => {
-											setLoginLoading(false);
-										}, 1000);
-									}}
-								>
-									Log In
-								</Button>
-							</div>
-						</Grid>
-					</Tooltip>
+					<Grid>
+						<div className="block">
+							<Button
+								type="default"
+								auto
+								scale={0.7}
+								placeholder={undefined}
+								onPointerEnterCapture={undefined}
+								onPointerLeaveCapture={undefined}
+								icon={<LogIn />}
+								loading={loginLoading}
+								onClick={() => {
+									setLoginLoading(true);
+									setState(true);
+									// window.location.href = "https://app.growth-engineering.io";
+								}}
+							>
+								Log In
+							</Button>
+						</div>
+					</Grid>
 
 					<Grid>
 						<Button
